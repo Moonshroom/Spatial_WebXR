282c679150dcf4f3dda7b82178706256a21f8a5c
Mateusz Orylski <51463915+Moonshroom@users.noreply.github.com>
2021-01-22 01:37:13 +0100

    Move_files_to_seperate_folders

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 //functions
 let loadMainPage = () => {
     if (window.location.pathname !== "/index.html"){
-        window.location = "index.html"
+        window.location = "/index.html"
     };  
 };
 let loadVRexamples = () => {
     if (window.location.pathname !== "/_VR_index.html"){
-        window.location = "_VR_index.html"
+        window.location = "/transition/_VR_index.html"
     };  
 };
 let loadARexamples = () => {
-    if (window.location.pathname !== "/_AR_index.html"){
-        window.location = "_AR_index.html"
+    if (window.location.pathname !== "/transition/_AR_index.html"){
+        window.location = "/transition/_AR_index.html"
     };  
 };
 let loadWGLexamples = () => {
-    if (window.location.pathname !== "/_WebGL_index.html"){
-        window.location = "_WebGL_index.html"
+    if (window.location.pathname !== "/transition/_WebGL_index.html"){
+        window.location = "/transition/_WebGL_index.html"
     };  
 };
 
