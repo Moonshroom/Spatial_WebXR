759ddd8bf927cfd5c093b9d0525143bdf43561de
Mateusz Orylski <51463915+Moonshroom@users.noreply.github.com>
2021-01-22 02:24:26 +0100

    Fix_paths_gh_pages

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
 //functions
 let loadMainPage = () => {
     if (window.location.pathname !== "/index.html"){
-        window.location = "/index.html"
+        // window.location = "/index.html"
+        window.location = "/Spatial_WebXR/index.html" //githubpages
     };  
 };
 let loadVRexamples = () => {
     if (window.location.pathname !== "/VR_index.html"){
-        window.location = "/transition/VR_index.html"
+        // window.location = "/transition/VR_index.html"        
+        window.location = "/Spatial_WebXR/transition/VR_index.html" //githubpages
     };  
 };
 let loadARexamples = () => {
     if (window.location.pathname !== "/transition/AR_index.html"){
-        window.location = "/transition/AR_index.html"
+        // window.location = "/transition/AR_index.html"
+        window.location = "/Spatial_WebXR/transition/AR_index.html" //githubpages
     };  
 };
 let loadWGLexamples = () => {
     if (window.location.pathname !== "/transition/WebGL_index.html"){
-        window.location = "/transition/WebGL_index.html"
+        // window.location = "/transition/WebGL_index.html"
+        window.location = "/Spatial_WebXR/transition/WebGL_index.html" //githubpages
+        
     };  
 };
 
 let loadVrExOne = () => {
     if (window.location.pathname !== "/examples/example_01/ex_01.html"){
-        window.location = "/examples/example_01/ex_01.html"
+        // window.location = "/examples/example_01/ex_01.html"
+        window.location = "/Spatial_WebXR/examples/example_01/ex_01.html" //githubpages
     };  
 };
 
