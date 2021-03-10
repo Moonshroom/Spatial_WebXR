4ea1b91ce822486ef4c965182b20888b1b92f3f4
Mateusz Orylski <51463915+Moonshroom@users.noreply.github.com>
2021-03-10 22:56:57 +0100

    Update VRexample 3
    Add 3DModel of  Park Bambrow Poznanskich
    Update scene properties

--- index.js
+++ index.js
@@ -74,6 +74,11 @@ let loadVrExTwo = () => {
 	}
 };
 
+let loadVrExThree = () => {
+	if (window.location.pathname !== '/examples/example_03/ex_03.html') {
+		window.location = '/Spatial_WebXR/examples/example_03/ex_03.html';
+	}
+};
 let btnVrOne;
 btnVrOne = document.getElementById('btn-enterVR-one');
 btnVrOne.addEventListener('click', loadVrExOne);
@@ -81,3 +86,7 @@ btnVrOne.addEventListener('click', loadVrExOne);
 let btnVrTwo;
 btnVrTwo = document.getElementById('btn-enterVR-two');
 btnVrTwo.addEventListener('click', loadVrExTwo);
+
+let btnVrThree;
+btnVrThree = document.getElementById('btn-enterVR-three');
+btnVrThree.addEventListener('click', loadVrExThree);
