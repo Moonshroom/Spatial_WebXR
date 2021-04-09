121da8d76561b2a64dd8e1cfd5b9e331059654b6
Mateusz Orylski <51463915+Moonshroom@users.noreply.github.com>
2021-04-09 21:58:55 +0200

    Update version
    Github pages reload check

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ arElement.addEventListener('click', loadARexamples);
 
 //app version
 let appVersion = document.getElementById('app-version');
-appVersion.innerHTML = '0.8.8';
+appVersion.innerHTML = '0.8.9';
 //VR examples navigation
 let loadVrExOne = () => {
 	if (window.location.pathname !== '/examples/example_01/ex_01.html') {
