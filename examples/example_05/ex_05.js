18ddfc2a86328758143b545708c5bfb7346c8e97
Mateusz Orylski <51463915+Moonshroom@users.noreply.github.com>
2021-04-22 20:26:44 +0200

    New model loader for Ar
    Update model loader AR

--- examples/example_05/ex_05.js
+++ examples/example_05/ex_05.js
@@ -2,17 +2,18 @@ import * as THREE from '../libs/three/three.module.js';
 import { GLTFLoader } from '../libs/three/jsm/GLTFLoader.js';
 import { DRACOLoader } from '../libs/three/jsm/DRACOLoader.js';
 import { RGBELoader } from '../libs/three/jsm/RGBELoader.js';
-import { LoadingBar } from '../libs/LoadingBar.js';
 import { ARButton } from '../libs/ARButton.js';
+import { LoadingBar } from '../libs/LoadingBar.js';
 
 class App {
 	constructor() {
 		const container = document.createElement('div');
 		document.body.appendChild(container);
 
-		this.clock = new THREE.Clock();
-
 		this.loadingBar = new LoadingBar();
+		this.loadingBar.visible = false;
+
+		this.assetsPath = './assets/';
 
 		this.camera = new THREE.PerspectiveCamera(
 			70,
@@ -20,112 +21,57 @@ class App {
 			0.01,
 			20
 		);
-		this.camera.position.set(0, 0, 0);
+		this.camera.position.set(0, 1.6, 0);
 
 		this.scene = new THREE.Scene();
 
-		const ambient = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 2);
+		const ambient = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
 		ambient.position.set(0.5, 1, 0.25);
 		this.scene.add(ambient);
-
-		const light = new THREE.DirectionalLight();
-		light.position.set(0.2, 5, 1);
-		this.scene.add(light);
 
 		this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
 		this.renderer.setPixelRatio(window.devicePixelRatio);
 		this.renderer.setSize(window.innerWidth, window.innerHeight);
 		this.renderer.outputEncoding = THREE.sRGBEncoding;
 		container.appendChild(this.renderer.domElement);
-		// this.setEnvironment();
+		this.setEnvironment();
 
-		this.workingVec3 = new THREE.Vector3();
+		this.reticle = new THREE.Mesh(
+			new THREE.RingBufferGeometry(0.15, 0.2, 32).rotateX(-Math.PI / 2),
+			new THREE.MeshBasicMaterial()
+		);
 
-		this.initScene();
+		this.reticle.matrixAutoUpdate = false;
+		this.reticle.visible = false;
+		this.scene.add(this.reticle);
+
 		this.setupXR();
 
 		window.addEventListener('resize', this.resize.bind(this));
 	}
 
-	// setEnvironment() {
-	// 	const loader = new RGBELoader().setDataType(THREE.UnsignedByteType);
-	// 	const pmremGenerator = new THREE.PMREMGenerator(this.renderer);
-	// 	pmremGenerator.compileEquirectangularShader();
-
-	// 	const self = this;
-
-	// 	loader.load(
-	// 		'./assets/venice_sunset_1k.hdr',
-	// 		(texture) => {
-	// 			const envMap = pmremGenerator.fromEquirectangular(texture).texture;
-	// 			pmremGenerator.dispose();
-
-	// 			self.scene.environment = envMap;
-	// 		},
-	// 		undefined,
-	// 		(err) => {
-	// 			console.error('An error occurred setting the environment');
-	// 		}
-	// 	);
-	// }
-
-	resize() {
-		this.camera.aspect = window.innerWidth / window.innerHeight;
-		this.camera.updateProjectionMatrix();
-		this.renderer.setSize(window.innerWidth, window.innerHeight);
-	}
-
-	loadWngig() {
-		const loader = new GLTFLoader().setPath('./assets/');
-		const self = this;
-		let dracoLoader = new DRACOLoader();
-		dracoLoader.setDecoderPath('../libs/three/js/draco/');
-		loader.setDRACOLoader(dracoLoader);
-		// Load a glTF resource
-		loader.load(
-			// resource URL
-			'wngig_google.glb',
-			// called when the resource is loaded
-			function (gltf) {
-				self.mymesh = gltf.scene;
-				self.mymesh.position.set(0, -0.25, -1);
-				self.mymesh.scale.set(0.25, 0.25, 0.25);
-				self.scene.add(gltf.scene);
-				self.mymesh.name = 'wngig';
-				self.loadingBar.visible = false;
-				self.renderer.setAnimationLoop(self.render.bind(self));
-			},
-			// called while loading is progressing
-			function (xhr) {
-				self.loadingBar.progress = xhr.loaded / xhr.total;
-			},
-			// called when loading has errors
-			function (error) {
-				console.log('An error happened');
-			}
-		);
-	}
-
-	initScene() {
-		this.mixers = [];
-		this.collisionObjects = [];
-		this.loadWngig();
-	}
-
 	setupXR() {
 		this.renderer.xr.enabled = true;
 
-		const btn = new ARButton(this.renderer, {
-			sessionInit: { optionalFeatures: ['dom-overlay'], domOverlay: { root: document.body } },
-		});
+		if ('xr' in navigator) {
+			navigator.xr.isSessionSupported('immersive-ar').then((supported) => {
+				if (supported) {
+					const collection = document.getElementsByClassName('ar-button');
+				}
+			});
+		}
 
 		const self = this;
 
+		this.hitTestSourceRequested = false;
+		this.hitTestSource = null;
+
 		function onSelect() {
-			if (!self.action.isRunning()) {
-				self.action.time = 0;
-				self.action.enabled = true;
-				self.action.play();
+			if (self.chair === undefined) return;
+
+			if (self.reticle.visible) {
+				self.chair.position.setFromMatrixPosition(self.reticle.matrix);
+				self.chair.visible = true;
 			}
 		}
 
@@ -134,10 +80,148 @@ class App {
 
 		this.scene.add(this.controller);
 	}
+
+	resize() {
+		this.camera.aspect = window.innerWidth / window.innerHeight;
+		this.camera.updateProjectionMatrix();
+		this.renderer.setSize(window.innerWidth, window.innerHeight);
+	}
+
+	setEnvironment() {
+		const loader = new RGBELoader().setDataType(THREE.UnsignedByteType);
+		const pmremGenerator = new THREE.PMREMGenerator(this.renderer);
+		pmremGenerator.compileEquirectangularShader();
+
+		const self = this;
+
+		loader.load(
+			'./assets/venice_sunset_1k.hdr',
+			(texture) => {
+				const envMap = pmremGenerator.fromEquirectangular(texture).texture;
+				pmremGenerator.dispose();
+
+				self.scene.environment = envMap;
+			},
+			undefined,
+			(err) => {
+				console.error('An error occurred setting the environment');
+			}
+		);
+	}
+
+	showChair() {
+		this.initAR();
+
+		const loader = new GLTFLoader().setPath(this.assetsPath);
+		const self = this;
+		let dracoLoader = new DRACOLoader();
+		dracoLoader.setDecoderPath('../libs/three/js/draco/');
+		loader.setDRACOLoader(dracoLoader);
+		this.loadingBar.visible = true;
+
+		// Load a glTF resource
+		loader.load(
+			// resource URL
+			`wngig_google.glb`,
+			// called when the resource is loaded
+			function (gltf) {
+				self.scene.add(gltf.scene);
+				self.chair = gltf.scene;
+
+				self.chair.visible = false;
+
+				self.loadingBar.visible = false;
+
+				self.renderer.setAnimationLoop(self.render.bind(self));
+			},
+			// called while loading is progressing
+			function (xhr) {
+				self.loadingBar.progress = xhr.loaded / xhr.total;
+			},
+			// called when loading has errors
+			function (error) {
+				console.log('An error happened', error);
+			}
+		);
+	}
+
+	initAR() {
+		let currentSession = null;
+		const self = this;
+
+		const sessionInit = { requiredFeatures: ['hit-test'] };
+
+		function onSessionStarted(session) {
+			session.addEventListener('end', onSessionEnded);
+
+			self.renderer.xr.setReferenceSpaceType('local');
+			self.renderer.xr.setSession(session);
+
+			currentSession = session;
+		}
+
+		function onSessionEnded() {
+			currentSession.removeEventListener('end', onSessionEnded);
+
+			currentSession = null;
+
+			if (self.chair !== null) {
+				self.scene.remove(self.chair);
+				self.chair = null;
+			}
+
+			self.renderer.setAnimationLoop(null);
+		}
+
+		if (currentSession === null) {
+			navigator.xr.requestSession('immersive-ar', sessionInit).then(onSessionStarted);
+		} else {
+			currentSession.end();
+		}
+	}
+
+	requestHitTestSource() {
+		const self = this;
+
+		const session = this.renderer.xr.getSession();
+
+		session.requestReferenceSpace('viewer').then(function (referenceSpace) {
+			session.requestHitTestSource({ space: referenceSpace }).then(function (source) {
+				self.hitTestSource = source;
+			});
+		});
+
+		session.addEventListener('end', function () {
+			self.hitTestSourceRequested = false;
+			self.hitTestSource = null;
+			self.referenceSpace = null;
+		});
+
+		this.hitTestSourceRequested = true;
+	}
+
+	getHitTestResults(frame) {
+		const hitTestResults = frame.getHitTestResults(this.hitTestSource);
+
+		if (hitTestResults.length) {
+			const referenceSpace = this.renderer.xr.getReferenceSpace();
+			const hit = hitTestResults[0];
+			const pose = hit.getPose(referenceSpace);
+
+			this.reticle.visible = true;
+			this.reticle.matrix.fromArray(pose.transform.matrix);
+		} else {
+			this.reticle.visible = false;
+		}
+	}
+
 	render(timestamp, frame) {
-		const dt = this.clock.getDelta();
-		let wngig = this.scene.getObjectByName('wngig');
-		console.log(wngig);
+		if (frame) {
+			if (this.hitTestSourceRequested === false) this.requestHitTestSource();
+
+			if (this.hitTestSource) this.getHitTestResults(frame);
+		}
+
 		this.renderer.render(this.scene, this.camera);
 	}
 }
---
+++ examples/example_05/test_01.js
@@ -0,0 +1,145 @@
+import * as THREE from '../libs/three/three.module.js';
+import { GLTFLoader } from '../libs/three/jsm/GLTFLoader.js';
+import { DRACOLoader } from '../libs/three/jsm/DRACOLoader.js';
+import { RGBELoader } from '../libs/three/jsm/RGBELoader.js';
+import { LoadingBar } from '../libs/LoadingBar.js';
+import { ARButton } from '../libs/ARButton.js';
+
+class App {
+	constructor() {
+		const container = document.createElement('div');
+		document.body.appendChild(container);
+
+		this.clock = new THREE.Clock();
+
+		this.loadingBar = new LoadingBar();
+
+		this.camera = new THREE.PerspectiveCamera(
+			70,
+			window.innerWidth / window.innerHeight,
+			0.01,
+			20
+		);
+		this.camera.position.set(0, 0, 0);
+
+		this.scene = new THREE.Scene();
+
+		const ambient = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 2);
+		ambient.position.set(0.5, 1, 0.25);
+		this.scene.add(ambient);
+
+		const light = new THREE.DirectionalLight();
+		light.position.set(0.2, 5, 1);
+		this.scene.add(light);
+
+		this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+		this.renderer.setPixelRatio(window.devicePixelRatio);
+		this.renderer.setSize(window.innerWidth, window.innerHeight);
+		this.renderer.outputEncoding = THREE.sRGBEncoding;
+		container.appendChild(this.renderer.domElement);
+		// this.setEnvironment();
+
+		this.workingVec3 = new THREE.Vector3();
+
+		this.initScene();
+		this.setupXR();
+
+		window.addEventListener('resize', this.resize.bind(this));
+	}
+
+	// setEnvironment() {
+	// 	const loader = new RGBELoader().setDataType(THREE.UnsignedByteType);
+	// 	const pmremGenerator = new THREE.PMREMGenerator(this.renderer);
+	// 	pmremGenerator.compileEquirectangularShader();
+
+	// 	const self = this;
+
+	// 	loader.load(
+	// 		'./assets/venice_sunset_1k.hdr',
+	// 		(texture) => {
+	// 			const envMap = pmremGenerator.fromEquirectangular(texture).texture;
+	// 			pmremGenerator.dispose();
+
+	// 			self.scene.environment = envMap;
+	// 		},
+	// 		undefined,
+	// 		(err) => {
+	// 			console.error('An error occurred setting the environment');
+	// 		}
+	// 	);
+	// }
+
+	resize() {
+		this.camera.aspect = window.innerWidth / window.innerHeight;
+		this.camera.updateProjectionMatrix();
+		this.renderer.setSize(window.innerWidth, window.innerHeight);
+	}
+
+	loadWngig() {
+		const loader = new GLTFLoader().setPath('./assets/');
+		const self = this;
+		let dracoLoader = new DRACOLoader();
+		dracoLoader.setDecoderPath('../libs/three/js/draco/');
+		loader.setDRACOLoader(dracoLoader);
+		// Load a glTF resource
+		loader.load(
+			// resource URL
+			'wngig_google.glb',
+			// called when the resource is loaded
+			function (gltf) {
+				self.mymesh = gltf.scene;
+				self.mymesh.position.set(0, -0.25, -1);
+				self.mymesh.scale.set(0.25, 0.25, 0.25);
+				self.scene.add(gltf.scene);
+				self.mymesh.name = 'wngig';
+				self.loadingBar.visible = false;
+				self.renderer.setAnimationLoop(self.render.bind(self));
+			},
+			// called while loading is progressing
+			function (xhr) {
+				self.loadingBar.progress = xhr.loaded / xhr.total;
+			},
+			// called when loading has errors
+			function (error) {
+				console.log('An error happened');
+			}
+		);
+	}
+
+	initScene() {
+		this.mixers = [];
+		this.collisionObjects = [];
+		this.loadWngig();
+	}
+
+	setupXR() {
+		this.renderer.xr.enabled = true;
+
+		const btn = new ARButton(this.renderer, {
+			sessionInit: { optionalFeatures: ['dom-overlay'], domOverlay: { root: document.body } },
+		});
+
+		const self = this;
+
+		function onSelect() {
+			if (!self.action.isRunning()) {
+				self.action.time = 0;
+				self.action.enabled = true;
+				self.action.play();
+			}
+		}
+
+		this.controller = this.renderer.xr.getController(0);
+		this.controller.addEventListener('select', onSelect);
+
+		this.scene.add(this.controller);
+	}
+	render(timestamp, frame) {
+		const dt = this.clock.getDelta();
+		let wngig = this.scene.getObjectByName('wngig');
+		console.log(wngig);
+		this.renderer.render(this.scene, this.camera);
+	}
+}
+
+export { App };
