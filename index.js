259117f9b1bd64b1ca39c43238eea972e2dcec99
Mateusz Orylski <51463915+Moonshroom@users.noreply.github.com>
2021-04-26 14:32:14 +0200

    Udpade AR ex01 UI

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ if (mediaQuery.matches && window.location.pathname == '/Spatial_WebXR/AR_index.h
 
 //app version
 let appVersion = document.getElementById('app-version');
-appVersion.innerHTML = '0.9.92';
+appVersion.innerHTML = '1.0.02';
 //VR examples navigation
 let loadVrExOne = () => {
 	if (window.location.pathname !== '/examples/example_01/ex_01.html') {
